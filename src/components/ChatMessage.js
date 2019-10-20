f991464b9572c5024f196bd133381421c6387925
Jonathan <jonathanlimtiaco@gmail.com>
2019-10-20 12:08:17 -0700

    [Chat] Add images for chat (#24)

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -1,24 +1,54 @@
-import React, { Component } from "react"
-import { Alert, StyleSheet, TouchableOpacity, View } from "react-native"
+import React from "react"
+import { StyleSheet, View } from "react-native"
 
+import FlexImage from "./common/FlexImage"
 import Text from "./common/Text"
 
 import { DEVICE_WIDTH } from "../constants/dimensions"
 import { Colors, DEFAULT_PADDING, Fonts } from "../constants/style"
 
-const ChatMessage = ({ isMyMessage, message }) => {
-  return (
-    <View
-      style={[
-        styles.container,
-        isMyMessage ? styles.myMessage : styles.otherMessage,
-      ]}
-    >
-      <Text type={Fonts.CerealBook}>
-        {message.message}
-      </Text>
-    </View>
-  )
+const ChatMessage = ({ chatMessage, isMyMessage }) => {
+  const { image, message } = chatMessage
+  if (!image) {
+    return (
+      <View
+        style={[
+          styles.container,
+          isMyMessage ? styles.myMessage : styles.otherMessage,
+        ]}
+      >
+        <Text type={Fonts.CerealBook}>
+          {message}
+        </Text>
+      </View>
+    )
+  } else if (!message) {
+    return (
+      <FlexImage
+        source={{ uri: image }}
+        style={[styles.image, isMyMessage ? styles.myMessage : styles.otherMessage ]}
+      />
+    )
+  } else {
+    return (
+      <>
+        <View
+          style={[
+            styles.container,
+            isMyMessage ? styles.myMessage : styles.otherMessage,
+          ]}
+        >
+          <Text type={Fonts.CerealBook}>
+            {message}
+          </Text>
+        </View>
+        <FlexImage
+          source={{ uri: image }}
+          style={[styles.image, isMyMessage ? styles.myMessage : styles.otherMessage ]}
+        />
+      </>
+    )
+  }
 }
 
 export default ChatMessage
@@ -32,6 +62,12 @@ const styles = StyleSheet.create({
     marginHorizontal: DEFAULT_PADDING,
     maxWidth: DEVICE_WIDTH * 0.75,
     padding: DEFAULT_PADDING,
+  },
+  image: {
+    borderRadius: 6,
+    marginBottom: DEFAULT_PADDING,
+    marginHorizontal: DEFAULT_PADDING,
+    width: DEVICE_WIDTH / 2,
   },
   myMessage: {
     alignSelf: "flex-end",
---
+++ src/components/common/FlexImage.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react"
+import { Image, StyleSheet } from "react-native"
+
+import { DEVICE_WIDTH } from "../../constants/dimensions"
+import { Colors, DEFAULT_PADDING, Fonts } from "../../constants/style"
+
+const FlexImage = ({ source, style }) => {
+  const [aspectRatio, setAspectRatio] = useState(1)
+
+  useEffect(() => {
+    Image.getSize(
+      source.uri,
+      (width, height) => setAspectRatio(width / height),
+      (err) => console.log("## Image getSize:", err)
+    )
+  }, [source])
+
+  return (
+    <Image
+      resizeMode="contain"
+      source={source}
+      style={[styles.image, { aspectRatio }, style]}
+    />
+  )
+}
+
+export default FlexImage
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+  },
+})
