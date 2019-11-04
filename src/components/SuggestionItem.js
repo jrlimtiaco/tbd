5a1b26089f250c6ea019f351fdba531eef5cfc2f
Jonathan Limtiaco <jonathanlimtiaco@gmail.com>
2019-11-03 23:33:20 -0800

    [Suggestion] Add ability to delete suggestions

--- src/components/SuggestionItem.js
+++ src/components/SuggestionItem.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
-import { Alert, StyleSheet, TouchableOpacity, View } from "react-native"
+import { Alert, StyleSheet, TouchableOpacity, TouchableWithoutFeedback, View } from "react-native"
+import { connectActionSheet } from '@expo/react-native-action-sheet'
 import firebase from "firebase"
 
 import * as Icon from "@expo/vector-icons"
@@ -7,6 +8,7 @@ import Text from "./common/Text"
 
 import { Colors, DEFAULT_PADDING, Fonts } from "../constants/style"
 
+@connectActionSheet
 class SuggestionItem extends Component {
 
   _onPress = async () => {
@@ -31,10 +33,36 @@ class SuggestionItem extends Component {
     }
   }
 
+  _onLongPress = (suggestionId) => {
+    this.props.showActionSheetWithOptions({
+      options: ['Delete', 'Cancel'],
+      cancelButtonIndex: 1,
+      destructiveButtonIndex: 0,
+      },
+      async buttonIndex => {
+        if (buttonIndex === 0) {
+          const profile = await firebase
+            .firestore()
+            .collection("Users")
+            .doc(firebase.auth().currentUser.uid)
+            .get()
+          const { currentTrip } = profile.data()
+          await firebase.firestore()
+            .collection("Suggestions")
+            .doc(currentTrip)
+            .collection("suggestions")
+            .doc(suggestionId)
+            .delete()
+        }
+      }
+    )
+  }
+
   render() {
     const { suggestion } = this.props
     const isLiked = suggestion.likes.includes(firebase.auth().currentUser.uid)
     return (
+      <TouchableWithoutFeedback onLongPress={() => this._onLongPress(suggestion.id)}>
       <View style={styles.container}>
         <View style={styles.textContainer}>
           <Text size="large" style={styles.text} type={Fonts.CerealBook}>
@@ -53,6 +81,7 @@ class SuggestionItem extends Component {
           </Text>
         </View>
       </View>
+      </TouchableWithoutFeedback>
     )
   }
 }
