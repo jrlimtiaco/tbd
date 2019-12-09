d40d28ede0fec2a0999d49c8cbb77a8d54b13605
Jonathan Limtiaco <jonathanlimtiaco@gmail.com>
2019-12-08 22:15:52 -0800

    [Slideshow] Add slideshow for app

--- src/AuthHandler.js
+++ src/AuthHandler.js
@@ -1,15 +1,24 @@
 import React, { Component } from "react"
+import { AsyncStorage } from "react-native"
 
 import firebase from "firebase"
 import Loader from "./components/common/Loader"
 
-import { APP, AUTH } from "./constants/routes"
+import { SLIDESHOW_KEY } from "./components/Slideshow"
+import { APP, AUTH, SLIDESHOW } from "./constants/routes"
 
 export default class AuthHandler extends Component {
 
-  componentDidMount() {
+  async componentDidMount() {
+    const slideshowKey = await AsyncStorage.getItem(SLIDESHOW_KEY)
     firebase.auth().onAuthStateChanged(user => {
-      this.props.navigation.navigate(user ? APP : AUTH)
+      let route = AUTH
+      if (user && !slideshowKey) {
+        route = SLIDESHOW
+      } else if (user) {
+        route = APP
+      }
+      this.props.navigation.navigate(route)
     })
   }
 
